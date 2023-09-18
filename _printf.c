45241ad4779eef778946c967b774ba3a7bcf2333
OKOTB <osamaquotp@gmail.com>
2023-09-17 18:56:55 -0700

    add if condition

--- _printf.c
+++ _printf.c
@@ -22,6 +22,8 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
