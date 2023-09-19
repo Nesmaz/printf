4f945753705925433ba1b2d015877919af24729d
Nesmaz <nesmabdelazim@gmail.com>
2023-09-19 16:39:00 -0700

    flags

--- _process_format.c
+++ _process_format.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stddef.h>
 #include "main.h"
 /**
@@ -8,41 +6,24 @@
  * @format: The format string
  * @conversion_specifiers: Array of conversion specifiers
  * @args: List of arguments
- *
- * This function processes a format string.
- * It iterates over each character in the string.
- * If it encounters a '%', it checks the next character
- * against the list of conversion specifiers.
- * If it finds a match, it calls the corresponding handler function
- * and adds its return value to the count.
- * If it doesn't find a match and the next character is not a space,
- * it prints the '%' and the next character.
- * If the next character is a null byte, it returns -1.
- * For all other characters, it prints them as they are.
- *
  * Return: The count of characters printed
 */
 int process_format(const char *format,
-			specifier_t conversion_specifiers[],
-			va_list args)
+			specifier_t conversion_specifiers[], va_list args)
 {
-	int i, j, handler_return, count = 0;
+	int i, j, count = 0;
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; conversion_specifiers[j].specifier != NULL; j++)
+			i++;
+			if (is_flag(format[i], "-+ 0#"))
 			{
-				if (format[i + 1] == conversion_specifiers[j].specifier[0])
-				{
-					handler_return = conversion_specifiers[j].handler(args);
-					if (handler_return == -1)
-						return (-1);
-					count += handler_return;
-					break;
-				}
+				handle_flag(&format[i],&i);
+				i++;
 			}
+			j = handle_specifiers(format, i, conversion_specifiers, args, &count);
 			if (conversion_specifiers[j].specifier == NULL && format[i + 1] != ' ')
 			{
 				if (format[i + 1] != '\0')
@@ -50,18 +31,14 @@ int process_format(const char *format,
 					_putchar(format[i]);
 					_putchar(format[i + 1]);
 					count += 2;
-				}
-				else
-					return (-1);
+			} else
+				return (-1);
 			}
 			i++;
-		}
-		else
+		} else
 		{
 			_putchar(format[i]);
 			count++;
 		}
-	}
-	return (count);
+	} return (count);
 }
-
---
+++ handle_flags.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * handle_flag - Parses the format string and identifies which flags are set
+ * @format: The format string to parse
+ * @i: A pointer to the current position in the format string
+ * Return: An integer where each bit represents a different flag
+*/
+
+int handle_flag(const char *format, int *i)
+{
+	int flags = 0;
+
+	while (is_flag(*i, "-+ 0#"))
+	{
+		switch (format[*i])
+		{
+			case '-':
+				{
+					flags |= 1;
+					break;
+				}
+			case '+':
+				{
+					flags |= 2;
+					break;
+				}
+			case '0':
+				{
+					flags |= 4;
+					break;
+				}
+			case '#':
+				{
+					flags |= 8;
+					break;
+				}
+			case ' ':
+				{
+					flags |= 16;
+					break;
+				}
+			default:
+				{
+					return (flags);
+				}
+		}
+		(*i)++;
+	}
+	return (flags);
+}
